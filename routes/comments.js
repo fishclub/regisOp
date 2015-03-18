143aa8e7c952bcb5afb4851e7abf3d7bbcb84b60
first <kongkritasuwapongpatana@fico.com>
2015-03-18 18:04:52 +0700

    new page

---
+++ routes/comments.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var Comment = new Schema({
+	username : String,
+    content  : String,
+    created  : Date
+});
+
+mongoose.model( 'Comment', Comment );
