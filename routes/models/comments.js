ee6122822b9ab27218157922dd6d7931f3daecde
first <kongkritasuwapongpatana@fico.com>
2015-03-23 18:28:46 +0700

    add mongodb

--- routes/models/comments.js
+++ routes/models/comments.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/test', function (error) {
-    if (error) {
-        console.log(error);
-    }
-});
-
 var Schema   = mongoose.Schema;
 var CommentSchema = new Schema({
 	username : String,
@@ -17,19 +10,19 @@ var CommentSchema = new Schema({
 var Comment = mongoose.model('comment', CommentSchema);
 
 router.get('/commentlist', function (req, res){
-  Comment.find(function(err, comments){
-	  if (err) return console.error(err);
-      res.render( 'helloworld', {
-          title : 'Express Todo Example',
-          comments : comments
-      });
+	Comment.find(function(err, comments){
+		if (err) return console.error(err);
+		res.render( 'comments', {
+			title : 'Comments',
+			comments : comments
+		});
     });
 });
 
 router.get('/commentadd', function (req, res){
 	var objAdd = new Comment();
-		objAdd.username = "test";
-		objAdd.content = "hello";
+		objAdd.username = "test1";
+		objAdd.content = "hello1";
 		objAdd.save(function(err, comment) {
 			if (err) return console.error(err);
 			res.render('helloworld');
